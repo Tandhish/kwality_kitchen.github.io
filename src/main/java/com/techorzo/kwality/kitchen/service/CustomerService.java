d4aa67b20219c0ec452883c0f71cec5fc2a1825f
Indoriajs <gaminglegendsultimate@gmail.com>
2023-01-15 00:31:00 +0530

    > Added..small stuff

--- src/main/java/com/techorzo/kwality/kitchen/service/CustomerService.java
+++ src/main/java/com/techorzo/kwality/kitchen/service/CustomerService.java
@@ -1,11 +1,16 @@
 package com.techorzo.kwality.kitchen.service;
 
 import com.techorzo.kwality.kitchen.dao.main.CustomerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerService {
     private final CustomerDao customerDao;
 
-    public CustomerService(CustomerDao customerDao) {
+    @Autowired
+    public CustomerService(@Qualifier("SqlLite") CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
 }
