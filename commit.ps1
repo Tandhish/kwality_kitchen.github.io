e6949824c009c8ceb6530652ec611aea3d05b7e2
Ritank saxena <68276497+ritank1998@users.noreply.github.com>
2023-02-15 01:03:54 +0530

    ultrasingle

--- commit.ps1
+++ commit.ps1
@@ -5,10 +5,10 @@ $Message = Read-Host
 
 function  GIT_COMMIT() {
     git checkout front-end
-    git pull origin front-end
+    git pull https://github.com/Tandhish/kwality_kitchen.github.io.git front-end
     git add *
     git commit -m $Message
-    git push origin front-end
+    git push https://github.com/Tandhish/kwality_kitchen.github.io.git front-end
 }
 
 GIT_COMMIT
